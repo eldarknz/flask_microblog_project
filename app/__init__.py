cfe7b9bea452219d1c261424b543f6fcc273f319
Eldar <eldarknz@gmail.com>
2020-03-19 16:20:53 +0300

    added db, changed html pages

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,16 @@
 from flask import Flask
 from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
+# app
 app_inst = Flask(__name__)
+
+# config from config.py
 app_inst.config.from_object(Config)
 
-from app import routes
+# database
+db       = SQLAlchemy(app_inst)
+migrate  = Migrate(app_inst, db)
+
+from app import routes, models
--- app/routes.py
+++ app/routes.py
@@ -1,23 +1,31 @@
 # -*- coding: utf-8 -*-
-from flask import render_template
+from flask import render_template, flash, redirect, url_for
 from app import app_inst
+from app.forms import LoginForm
+
 
 @app_inst.route('/')
 @app_inst.route('/index')
 def index():
-    user = {'username': 'Miguel Soldado'}
+    user = {'username': 'Miguel'}
     posts = [
         {
             'author': {'username': 'John'},
-            'body'  : 'Beautiful day in Portland!'
+            'body': 'Beautiful day in Portland!'
         },
         {
             'author': {'username': 'Susan'},
-            'body'  : 'The Avengers movie was so cool!'
-        },
-        {
-            'author': {'username': 'Ипполит'},
-            'body'  : 'Какая гадость эта ваша заливная рыба!!'
+            'body': 'The Avengers movie was so cool!'
         }
     ]
     return render_template('index.html', title='Home', user=user, posts=posts)
+
+
+@app_inst.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(
+            form.username.data, form.remember_me.data))
+        return redirect(url_for('index'))
+    return render_template('login.html',  title='Sign In', form=form)
