30e32dbfd54ee05c8f3fd0507f79eb351d5e8eeb
Eldar <eldarknz@gmail.com>
2020-03-20 15:40:56 +0300

    work

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_login import LoginManager
 
 # app
 app_inst = Flask(__name__)
@@ -12,5 +13,9 @@ app_inst.config.from_object(Config)
 # database
 db       = SQLAlchemy(app_inst)
 migrate  = Migrate(app_inst, db)
+
+# login
+login            = LoginManager(app_inst)
+login.login_view = 'login'
 
 from app import routes, models
