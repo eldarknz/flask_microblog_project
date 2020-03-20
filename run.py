5f7440df891597daa9efd26b6064251e1a536854
Eldar <eldarknz@gmail.com>
2020-03-20 12:12:02 +0300

    work

--- run.py
+++ run.py
@@ -1,1 +1,6 @@
-from app import app_inst
+from app import app_inst, db
+from app.models import User, Post
+
+@app_inst.shell_context_processor
+def make_shell_context():
+    return {'db': db, 'User': User, 'Post': Post}
